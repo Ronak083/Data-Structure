49c6eef3880b59b52039c1cdb5c52ac6643bc551
ronak083 <ronakgupta083@gmail.com>
2023-07-15 12:44:28 +0530

    15/07 - LinkedList Questions

---
+++ src/Data_Structures/LinkedList/Questions/Main.java
@@ -0,0 +1,33 @@
+package Data_Structures.LinkedList.Questions;
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.createLL(2);
+        ll.insertNode(3);
+        ll.insertNode(2);
+        ll.insertNode(4);
+        ll.insertNode(3);
+        ll.insertNode(7);
+        ll.insertNode(5);
+        ll.traversalLL();
+
+        Questions q = new Questions();
+
+        //Delete Duplicates
+        //q.deleteDups(ll);
+        //ll.traversalLL();
+
+        //return Nth to Last
+        //System.out.println(q.nthToLast(ll,2).value);
+
+        //Partition
+        LinkedList t = q.partition(ll,4);
+        t.traversalLL();
+
+
+
+
+    }
+
+
+}
