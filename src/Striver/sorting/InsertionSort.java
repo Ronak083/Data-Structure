65188c3d49b66be51464719a8a5934c06a26cc57
ronak083 <ronakgupta083@gmail.com>
2023-09-07 00:04:30 +0530

    06/09 - Adding InsertionSort by Stirver

---
+++ src/Striver/sorting/InsertionSort.java
@@ -0,0 +1,31 @@
+package Striver.sorting;
+
+public class InsertionSort {
+    public static void sort(int[] arr){
+        /*Select an element in each iteration from the unsorted array(using a loop).
+Place it in its corresponding position in the sorted part and shift the remaining elements accordingly (using an inner loop and swapping).
+The “inner while loop” basically shifts the elements using swapping.*/
+        for (int i = 0;i<arr.length;i++){
+            int j = i;
+
+            while (j > 0 && arr[j-1] > arr[j]){
+                int temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
+        }
+        /*Time complexity: O(N2/2 + n/2) ~= O(N2), (where N = size of the array), for the worst, and average cases.
+        Space Complexity: O(1)*/
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 8, 9, 3, 6, 7};
+
+        sort(arr);
+
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
